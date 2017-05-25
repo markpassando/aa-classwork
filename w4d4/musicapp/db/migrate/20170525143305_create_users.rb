e44c3444396d5914a9197f14a9e4b688b318cfe7
Mark Passando <MarkPassando@gmail.com>
2017-05-25 17:57:56 -0400

    Add classwork Music App with Auth

--- w4d4/musicapp/db/migrate/20170525143305_create_users.rb
+++ w4d4/musicapp/db/migrate/20170525143305_create_users.rb
@@ -1,11 +1,14 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :password_digest
-      t.string :session_token
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
 
       t.timestamps null: false
     end
+
+    add_index(:users, :email)
+    add_index(:users, :session_token)
   end
 end
